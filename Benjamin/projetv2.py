2c6e57cb394d76cebf209ea38bbe13d2f56d5c57
benblais <ben.blais@laposte.net>
2018-04-25 14:40:41 +0200

    update

--- Benjamin/projetv2.py
+++ Benjamin/projetv2.py
@@ -1,7 +1,24 @@
  # coding: utf-8
 from Tkinter import *
-#import Tkinter, Tkconstants, tkFileDialog
-import tkFileDialog
+import tkFileDialog  ##Permet de Charger les fichiers
+from matplotlib.figure import *
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+import matplotlib.pyplot as plt
+
+
+#####Fenetre principale
+app = Tk()
+app.title("Classification neuronale") # donne un titre à la fenêtre
+app.geometry("800x600+600+300") # place la fenêtre
+app.configure(background='blue')#fond du programme
+
+
+
+############# Variables
+#global variableatester
+#variableatester=StringVar()
+#variableatester.set('True')
+variableatester=IntVar()
 
 ######Definitions
 
@@ -9,35 +26,40 @@ import tkFileDialog
 def Chargementtest(): #### Permet de retourner le chemin absolue permet l'acces au fichier
 	name = tkFileDialog.askopenfilename(initialdir = "/",title = "Selection du fichier test",filetypes = (("Fichier csv","*.csv"),("Fichier texte","*.txt"),("Tous les fichiers","*.*")))
 	print (name)
-	return (name)
+	return (name) #Retourne le chemin absolue du fichier
+	
+
+
 ######Recuperer cette putain d'entrée<<<<<<<<<<<<<<<<
 
-
 def Chargemententrainement():
+	global variableatester
 	entrainement=Tk()
 	entrainement.title("Le fichier d'entraînement")
-	app.geometry("800x600+600+300")
+	entrainement.geometry("800x600+600+300")
 	textefichier="Avez-vous un jeu d'essai?"
 	fichiers=Label(entrainement,text=textefichier)
 	fichiers.pack()
-	global variableatester
-	variableatester=StringVar()
-	variableatester.set('True')
-	R1 = Radiobutton(entrainement, text="Option 1", variable=variableatester, value='False',command=resultatsappui)
+	variable2=IntVar()
+	#variableatester=0
+	#variableatester=StringVar()
+	#variableatester.set('False')
+	#global R1
+	#global R2
+	R1 = Radiobutton(entrainement, text="Oui", variable=variable2, value=1,command=resultatsappui)
 	R1.pack( anchor = W )
-	R2 = Radiobutton(entrainement, text="Option 2", variable=variableatester, value='False', command=resultatsappui)
+	R2 = Radiobutton(entrainement, text="Non", variable=variable2, value=0, command=resultatsappui)
 	R2.pack(anchor = W)
-	print variableatester
 	#radio0 = Radiobutton(entrainement, text="Appui prolonge", variable=variableatester, value=0, command=resultatsappui)
 	#radio1 = Radiobutton(entrainement, text="Oui", variable=variableatester, value=1, command=resultatsappui)
 	#radio2 = Radiobutton(entrainement, text="Non", variable=variableatester, value=2, command=resultatsappui)
 	#radio3 = Button(entrainement, text="Valider", command=entrainement.destroy,width=10)
 	#radio1.pack(side="top");radio2.pack(side="bottom");radio3.pack();radio0.pack()
-
-	entrainement.mainloop()
+	variableatester=variable2
+	print variable2,"Test"
 
 def resultatsappui():
-	print str(variableatester.get())
+	print (variableatester.get())
 	if variableatester==0:
 			nameentrainement = tkFileDialog.askopenfilename(initialdir = "/",title = "Selection du fichier entrainement",filetypes = (("Fichier csv","*.csv"),("Fichier texte","*.txt"),("Tous les fichiers","*.*")))
 			print (nameentrainement)
@@ -57,7 +79,7 @@ def Lanceranalyse():
 def presentation():
 	presentation=Tk()
 	presentation.title("Presentation")
-	app.geometry("800x600+600+300")
+	presentation.geometry("800x600+600+300")
 	textepresentation="Actuellement etudiant en master I à l'Université de Bordeaux,\n nous avons pour projet de classer des neurones en deux types : Type I et Type II. \n"
 	text=Label(presentation,text=textepresentation)
 	text.pack()
@@ -66,7 +88,7 @@ def presentation():
 def explicationlogiciel():
 	explication=Tk()
 	explication.title("Le logiciel")
-	app.geometry("1000x500+600+300")
+	explication.geometry("1000x500+600+300")
 	texteexplication="Présentation du logiciel : \n - Chargement du fichier test : Prend en entrée le fichier test au format .csv et .txt où l'analyse va être réalisé \n - Choisir le fichier d'entrainement : Si le client dispose d'un fichier d'entraînement ayant les types de neurones, il peut le charger sinon un jeu lui sera fourni \n - Paramètres : Concerne le reglage des paramètres à prendre un compte pour l'analyse \n  - Lancer : Permet de lancer l'analyse et d'y afficher les resultats dans une nouvelle fenetre"   ###A mettre en forme
 	text=Label(explication, text=texteexplication)
 	text.pack()
@@ -76,21 +98,27 @@ def explicationlogiciel():
 def alert():#####Commande bouton du haut
     showinfo("fonctionne")
 
-
-
-#####Fenetre principale
-app = Tk()
-app.title("Classification neuronale") # donne un titre à la fenêtre
-app.geometry("800x600+600+300") # place la fenêtre
+def Lanceranalyse():
+	Analyse=Tk()
+	Analyse.title("Les résultats")
+	Analyse.geometry("1200x800+600+300")
+	titretext=Label(Analyse, text="LES RESULTATS", fg="Black", bg="Grey")
+	titretext.place(x=500, y=0, width=200, height=50)
+	name = ['TypeI', 'TypeII'] ##### Nom des parties
+	data = [5000, 6000] ##### Nombre de type I et nombre de type 2
+	explode=(0, 0.2) #### Explosion des deux parties (separation)
+	plt.pie(data, explode=explode, labels=name, autopct='%1.1f%%', startangle=90, shadow=True) #####autopct = pourcentage réel par rapport à celui indique, axis = equal permet un diagramme circulaire
+	plt.axis('equal')
+	plt.show('Test')
 
 
 ######## Creation de tous les boutons de chargement
-text=Label(app, text="Classification Neuronale", fg="Black", bg="white")   #####Mettre en gras et en gros (titre)
-b1= Button(app, text="Chargement du fichier test", command=Chargementtest, width=25)
-b2= Button(app, text="Choisir le fichier d'entraînement", fg="black", command=Chargemententrainement,width=25)
-b3= Button(app, text="Paramètres", fg="black",command=Parametres,width=25)
-b4= Button(app, text="Lancer", fg="black", command=Lanceranalyse)
-b5= Button(app, text="Quitter", fg="black", command=app.destroy,width=10)
+text=Label(app, text="Classification Neuronale", fg="White", bg="Blue")   #####Mettre en gras et en gros (titre)
+b1= Button(app, text="Chargement du fichier test",fg="Black", bg="White", command=Chargementtest, width=25)
+b2= Button(app, text="Choisir le fichier d'entraînement", fg="Black", bg="White", command=Chargemententrainement,width=25)
+b3= Button(app, text="Paramètres", fg="Black",bg="White", command=Parametres,width=25)
+b4= Button(app, text="Lancer", fg="Black",bg="White", command=Lanceranalyse)
+b5= Button(app, text="Quitter", fg="Black",bg="White", command=app.destroy,width=10)
 b1.place(x=300, y=100, width=200, height=40)
 b2.place(x=300, y=170, width=200, height=40)
 b3.place(x=300, y=240, width=200, height=40)
@@ -128,4 +156,9 @@ app.config(menu=menubar)
 app.mainloop()
 
 
-############# Variables
+
+
+
+
+
+
