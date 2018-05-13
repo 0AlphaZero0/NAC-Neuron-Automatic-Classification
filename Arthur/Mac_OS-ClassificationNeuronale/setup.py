86c9190cff43f4eaef9d3ffe5dc60726103202ed
0AlphaZero0 <the-prodigy-moi@live.fr>
2018-05-13 19:14:28 +0200

    update

--- Arthur/Mac_OS-ClassificationNeuronale/setup.py
+++ Arthur/Mac_OS-ClassificationNeuronale/setup.py
@@ -1,5 +1,6 @@
 from setuptools import setup
 from distutils.core import setup
+import distutils.sysconfig as c
 import py2app
 import codecs # Allows to load a file containing UTF-8 characters
 import matplotlib.pyplot as plt # Allows to create graphics, especially those in 3D
@@ -27,8 +28,9 @@ setup(
     name = "Neuron Classification",
     version = "1",
     description = "Classification of neurons through Machine Learning",
-    console = ["projetfinal.py"],
-    data_files=matplotlib.get_py2exe_datafiles(),
-    options={"py2exe":{r'includes':[r'scipy.sparse.csgraph._validation',r'scipy.linalg.cython_blas',r'scipy.linalg.cython_lapack', r'scipy.special._ufuncs_cxx',r'scipy._lib.messagestream',r'scipy._lib.messagestream',r'sklearn.utils.lgamma',r'sklearn.utils.weight_vector',r'sklearn.neighbors.typedefs',r'os'],'py2app':OPTIONS}},
+    #console = ["projetfinal.py"],
+    #data_files=matplotlib.get_py2exe_datafiles(),
+    options={"py2exe":{'py2app':OPTIONS,'includes':r'libpython2.7.dylib'}},
     setup_requires=['py2app'],
 )
+#r'includes':[r'scipy.sparse.csgraph._validation',r'scipy.linalg.cython_blas',r'scipy.linalg.cython_lapack', r'scipy.special._ufuncs_cxx',r'scipy._lib.messagestream',r'scipy._lib.messagestream',r'sklearn.utils.lgamma',r'sklearn.utils.weight_vector',r'sklearn.neighbors.typedefs',r'os'],
--- Arthur/Mac_OS-ClassificationNeuronale/venv/bin/activate.fish
+++ Arthur/Mac_OS-ClassificationNeuronale/venv/bin/activate.fish
@@ -36,7 +36,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/alphazero/Mac_OS-ClassificationNeuronale/venv"
+set -gx VIRTUAL_ENV "/home/alphazero/Documents/GitHub/Projet-Neuro/Arthur/Mac_OS-ClassificationNeuronale/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
